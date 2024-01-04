22d67cfce7c0a025837f0f19bacedcde633a8a73
Anjali <octal.anjalip@gmail.com>
2024-01-04 10:40:18 +0530

    127: create save post feature

--- myblog11/src/main/java/com/myblog/myblog11/Myblog11Application.java
+++ myblog11/src/main/java/com/myblog/myblog11/Myblog11Application.java
@@ -3,11 +3,13 @@ package com.myblog.myblog11;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class Myblog11Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Myblog11Application.class, args);
+		run(Myblog11Application.class, args);
 	}
 
 }
